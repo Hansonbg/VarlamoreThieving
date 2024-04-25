caaadd5d1a6a3b00642db7e4c256d4844a42ddda
Hansonbg <101675228+Hansonbg@users.noreply.github.com>
2024-04-25 13:15:33 -0400

    Minor refactor to plugin names after testing.

--- src/main/java/com/varlamorethieving/VarlamoreThievingPlugin.java
+++ src/main/java/com/varlamorethieving/VarlamoreThievingPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.varlamorethieving;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class VarlamoreThievingPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private VarlamoreThievingConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	VarlamoreThievingConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(VarlamoreThievingConfig.class);
 	}
 }
--- src/test/java/com/varlamorethieving/VarlamoreThievingPluginTest.java
+++ src/test/java/com/varlamorethieving/VarlamoreThievingPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.varlamorethieving;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class VarlamoreThievingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(VarlamoreThievingPlugin.class);
 		RuneLite.main(args);
 	}
 }
